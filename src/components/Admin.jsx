af6616b577907974c2a0cc4dc98930c46a8eb16a
Mahesh Kotkar <maheshkotkar2005@gmail.com>
2026-01-29 00:33:09 +0530

    Update Admin.jsx

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -19,7 +19,7 @@ const Admin = () => {
 
     const fetchContacts = async () => {
         try {
-            const response = await fetch('http://192.168.1.103:5000/api/contact');
+            const response = await fetch('https://portfolio-backend-mu-lemon.vercel.app/api/contact');
             const data = await response.json();
             if (data.success) {
                 setContacts(data.data);
